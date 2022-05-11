57055caf532c92d9ed2356068abc6d94891db18a
Filippo Argiolas <filippo.argiolas@gmail.com>
2022-05-11 20:48:40 +0200

    add docstrings here and there
    also make query attributes a little more general working on a single
    device instead of a list... not sure why I was doing it that way, must
    be some old code that survived revisions

--- tb-download.py
+++ tb-download.py
@@ -29,8 +29,35 @@ logging.basicConfig()
 logger = logging.getLogger("tb-download")
 logger.setLevel(logging.DEBUG)
 
+
 class TBDownload(object):
+    """Minimal client to download timeseries data from a ThingsBoard instance.
+
+    A few Python bindings for TB REST APIs, just the few needed to
+    enumerate assets and devices and download timeseries data. Not
+    meant for anything serious and never really tested outside our
+    specific ThingsBoard instance.
+
+    ThingsBoard has different APIs for different kind of users
+    (Tenant Admins and Customers) even when they do basically the same
+    things (e.g. enumerate devices the user can access). Then there's
+    a special user for the Public dashboards, it's a Customer without
+    credentials, but not all customer entry-points work... Here we try
+    to abstract all this away, at least for the few entry-points we
+    use.
+
+    :param url: address of the thingsboard instance
+    :param public_id: CustomerId of the Public user, from the public dashboard url
+    :param username: Customer or Tenant username
+    :param password: Customer or Tenant password
+
+    Note: if both public_id and credentials are provided public_id is
+    preferred and credentials are silently ignored.
+
+    """
+
     def __init__(self, url, public_id=None, username=None, password=None):
+        """Initialize class arguments."""
         self.url = url
         self.public_id = public_id
         self.username = username
@@ -57,6 +84,7 @@ class TBDownload(object):
             sys.exit(1)
 
     def login(self):
+        """Login with credentials provided at init."""
         if self.public_id:
             r = self._post(f'{self.url}/api/auth/login/public',
                            json={'publicId': self.public_id})
@@ -78,6 +106,11 @@ class TBDownload(object):
             self.user_id = r.json()['customerId']['id']
 
     def get_assets(self, page_size=20, page=0):
+        """Enumerate assets available for current user.
+
+        :param page_size: assets per page
+        :param page: current page
+        """
         if self.user_authority == "TENANT_ADMIN":
             r = self._get(f'{self.url}/api/tenant/assets',
                           headers=self.auth_headers,
@@ -91,6 +124,10 @@ class TBDownload(object):
         return r.json()
 
     def get_asset_devices(self, asset):
+        """Enumerate devices contained in selected asset.
+
+        :param asset: asset as returned by `get_assets()`
+        """
         entity_id = asset['id']['id']
         entity_type = asset['id']['entityType']
 
@@ -111,22 +148,24 @@ class TBDownload(object):
 
         return devs
 
-    def query_attributes(self, devices, query="", attributes=""):
-        attrs = []
-        try:
-            dev = [d for d in devices if query in d['name']][0]
-
-            entity_id = dev['id']['id']
-            entity_type = dev['id']['entityType']
-            r = self._get(f'{self.url}/api/plugins/telemetry/{entity_type}/{entity_id}/values/attributes',
-                          params={'keys': attributes},
-                          headers=self.auth_headers)
-            return r.json()
-
-        except IndexError:
-            pass
+    def query_attributes(self, device, attributes=""):
+        """Query attributes from a device."""
+        entity_id = device['id']['id']
+        entity_type = device['id']['entityType']
+        r = self._get(f'{self.url}/api/plugins/telemetry/{entity_type}/{entity_id}/values/attributes',
+                      params={'keys': attributes},
+                      headers=self.auth_headers)
+        return r.json()
 
     def get_devices(self, page_size=30, page=0, text_search=""):
+        """Enumerate devices.
+
+        Enumerate the devices current user can access.
+
+        :params page_size: devices per page
+        :params page: current page
+        :text_search: filter devices by name
+        """
         if self.user_authority == "TENANT_ADMIN":
             r = self._get(f'{self.url}/api/tenant/devices',
                           headers=self.auth_headers,
@@ -139,9 +178,51 @@ class TBDownload(object):
                                   'textSearch': text_search})
         return r.json()
 
+    def get_timeseries(self, device, keys, start_ts, end_ts, limit=86400*10):
+        """Retrieve time series for a device in the desired interval.
+
+        :param device: desired device
+        :param keys: data columns to download (empty string to get them all)
+        :param start_ts: interval start timestamp (in seconds)
+        :param end_ts: interval end timestamp (in seconds)
+        :param limit: maximum number of rows to retrieve
+        :return: a pandas dataframe
+        """
+        entity_id = device['id']['id']
+        entity_type = device['id']['entityType']
+        r = self._get(f'{self.url}/api/plugins/telemetry/{entity_type}/{entity_id}/values/timeseries',
+                      headers=self.auth_headers,
+                      params={'keys': ','.join(keys),
+                              'limit': limit,
+                              'startTs': int(start_ts * 1000),
+                              'endTs': int(end_ts * 1000)})
+
+        if not r.json():
+            logger.warning("no data for selected interval")
+            return None
+
+        dfs = []
+        for k in r.json().keys():
+            df = pd.DataFrame.from_dict(r.json()[k])
+            df.set_index("ts", drop=True, inplace=True)
+            df.columns = [k]
+            dfs.append(df.sort_index())
+
+        return pd.concat(dfs, join="outer", axis=1)
 
     def save_timeseries(self, device, start_date, end_date, prefix=""):
-        """Retrieve timeseries from ThingsBoard one day at the time and saves them to a CSV file."""
+        """Save timeseries data from selected interval in CSV file.
+
+        Download timeseries data from selected interval one day at the
+        time using `get_timeseries`, concat resulting dataframes and
+        save everything to a CSV file.
+
+        :param device: desired device
+        :param start_date: interval start, python datetime object
+        :param end_date: interval end, python datetime object
+        :param prefix: filename prefix
+
+        """
         entity_id = device['id']['id']
         entity_type = device['id']['entityType']
         r = self._get(f'{self.url}/api/plugins/telemetry/{entity_type}/{entity_id}/keys/timeseries',
@@ -177,30 +258,6 @@ class TBDownload(object):
             df.to_csv(fname, mode="a" if append else "w", columns=keys, header=not append)
 
             append = True
-
-    def get_timeseries(self, device, keys, start_ts, end_ts):
-        """Get timeseries data from ThingsBoard using REST API and stores it into a Pandas dataframe."""
-        entity_id = device['id']['id']
-        entity_type = device['id']['entityType']
-        r = self._get(f'{self.url}/api/plugins/telemetry/{entity_type}/{entity_id}/values/timeseries',
-                      headers=self.auth_headers,
-                      params={'keys': ','.join(keys),
-                              'limit': 86400 * 3,
-                              'startTs': int(start_ts * 1000),
-                              'endTs': int(end_ts * 1000)})
-
-        if not r.json():
-            logger.warning("no data for selected interval")
-            return None
-
-        dfs = []
-        for k in r.json().keys():
-            df = pd.DataFrame.from_dict(r.json()[k])
-            df.set_index("ts", drop=True, inplace=True)
-            df.columns = [k]
-            dfs.append(df.sort_index())
-
-        return pd.concat(dfs, join="outer", axis=1)
 
 
 if __name__ == '__main__':
@@ -248,12 +305,16 @@ if __name__ == '__main__':
             devs = client.get_asset_devices(asset)
             logger.info("devices: {}".format(", ".join([d['name'] for d in devs])))
 
-            attrs = client.query_attributes(devs, query="-gps",
-                                            attributes="station_name,station_location,active,lastActivityTime")
-            for attr in attrs:
-                if "Time" in attr["key"]:
-                    attr["value"] = dt.datetime.fromtimestamp(attr["value"] / 1000.)
-                logger.info(f'{attr["key"]}: {attr["value"]}')
+            try:
+                dev = [d for d in devs if "-gps" in d['name']][0]
+
+                attrs = client.query_attributes(dev, attributes="station_name,station_location,active,lastActivityTime")
+                for attr in attrs:
+                    if "Time" in attr["key"]:
+                        attr["value"] = dt.datetime.fromtimestamp(attr["value"] / 1000.)
+                    logger.info(f'{attr["key"]}: {attr["value"]}')
+            except IndexError:
+                pass
 
         exit()
 
