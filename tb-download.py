7aec0711683758c5688f1ad8dee516ee206fe3c1
Filippo Argiolas <filippo.argiolas@ca.infn.it>
2022-05-11 15:06:40 +0200

    refactor to remove tb_rest_client dependency
    Directly call TB Rest API so we don't have to depend from the good but
    often buggy and not actively maintained thingsboard-python-rest-client.
    
    Still missing tenant login and tenant api variants.

--- tb-download.py
+++ tb-download.py
@@ -17,103 +17,168 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-import argparse
-import functools
-import logging
-
-from tb_rest_client.rest_client_ce import RestClientCE
-from tb_rest_client.rest import ApiException
-
+import sys
+import requests
 import datetime as dt
+import logging
 import pandas as pd
+import argparse
+
 
 logging.basicConfig()
 logger = logging.getLogger("tb-download")
 logger.setLevel(logging.DEBUG)
 
+class TBDownload(object):
+    def __init__(self, url, public_id=None, username=None, password=None):
+        self.url = url
+        self.public_id = public_id
+        self.username = username
+        self.password = password
 
-def save_timeseries(client, device, start_date, end_date, prefix=""):
-    """Retrieve timeseries from ThingsBoard one day at the time and saves them to a CSV file."""
-    keys = rest_client.get_timeseries_keys_v1(dev.id.entity_type, dev.id)
-    delta = dt.timedelta(days=1)
-    total_timespan = end_date - start_date
-    orig_start_date = start_date
+    # poor error handling but useful for debugging: raise fatal
+    # exceptions for every error, including http ones
+    def _get(self, *args, **kwargs):
+        try:
+            r = requests.get(*args, **kwargs)
+            r.raise_for_status()
+            return r
+        except requests.exceptions.RequestException:
+            logger.exception('an exception occured during GET')
+            sys.exit(1)
 
-    fname = f"{prefix}-{device.id.id}.csv"
+    def _post(self, *args, **kwargs):
+        try:
+            r = requests.post(*args, **kwargs)
+            r.raise_for_status()
+            return r
+        except requests.exceptions.RequestException:
+            logger.exception('an exception occured during POST')
+            sys.exit(1)
 
-    logger.info(f"saving to {fname}")
+    def login(self):
+        r = self._post(f'{self.url}/api/auth/login/public',
+                       json={'publicId': self.public_id})
 
-    # create the csv in write mode in the first iteration
-    # append in the following ones
-    # also only enable header in the first loop
-    append = False
+        self.token = r.json()['token']
+        self.auth_headers = {'X-Authorization': f'Bearer {self.token}'}
 
-    while start_date <= end_date:
-        interval_start = start_date
-        interval_end = interval_start + delta
-        span = start_date - orig_start_date
-        start_date = interval_end
+    def get_assets(self, page_size=20, page=0):
+        r = self._get(f'{self.url}/api/customer/{self.public_id}/assets',
+                      headers=self.auth_headers,
+                      params={'pageSize': page_size, 'page': page})
 
-        logger.info(f"{device.name}: fetching day {span.days+1} of {total_timespan.days+1} (from {interval_start.isoformat()} to {interval_end.isoformat()})")
+        return r.json()
 
-        df = get_timeseries(client, device, keys, interval_start.timestamp(), interval_end.timestamp())
-        if df is None:
-            continue
+    def get_asset_devices(self, asset):
+        entity_id = asset['id']['id']
+        entity_type = asset['id']['entityType']
 
-        df.to_csv(fname, mode="a" if append else "w", columns=keys, header=not append)
+        r = self._get(f'{self.url}/api/relations',
+                      headers=self.auth_headers,
+                      params={'fromId': entity_id,
+                              'fromType': entity_type,
+                              'relationType': 'Contains'})
+        relations = r.json()
 
-        append = True
+        devs = []
+        for rel in relations:
+            to_id = rel['to']['id']
+            r = self._get(f'{self.url}/api/device/{to_id}',
+                          headers=self.auth_headers)
+
+            devs.append(r.json())
+
+        return devs
+
+    def query_attributes(self, devices, query="", attributes=""):
+        attrs = []
+        try:
+            dev = [d for d in devices if query in d['name']][0]
+
+            entity_id = dev['id']['id']
+            entity_type = dev['id']['entityType']
+            r = self._get(f'{self.url}/api/plugins/telemetry/{entity_type}/{entity_id}/values/attributes',
+                          params={'keys': attributes},
+                          headers=self.auth_headers)
+            return r.json()
+
+        except IndexError:
+            pass
+
+    def get_devices(self, page_size=30, page=0, text_search=""):
+        r = self._get(f'{self.url}/api/customer/{self.public_id}/devices',
+                      headers=self.auth_headers,
+                      params={'pageSize': page_size, 'page': page})
+
+        return r.json()
 
 
-def get_timeseries(client, device, keys, start_ts, end_ts):
-    """Get timeseries data from ThingsBoard using REST API and stores it into a Pandas dataframe."""
-    ts = client.get_timeseries(device.id.entity_type, device.id, ','.join(keys),
-                               limit=86400 * 2,
-                               start_ts=int(start_ts * 1000),
-                               end_ts=int(end_ts * 1000))
-    if (len(ts.keys()) == 0):
-        logger.warning("no data for selected interval")
-        return None
+    def save_timeseries(self, device, start_date, end_date, prefix=""):
+        """Retrieve timeseries from ThingsBoard one day at the time and saves them to a CSV file."""
+        entity_id = device['id']['id']
+        entity_type = device['id']['entityType']
+        r = self._get(f'{self.url}/api/plugins/telemetry/{entity_type}/{entity_id}/keys/timeseries',
+                      headers=self.auth_headers)
 
-    dfs = []
-    for k in keys:
-        df = pd.DataFrame.from_dict(ts[k])
-        df.set_index("ts", drop=True, inplace=True)
-        df.columns = [k]
-        dfs.append(df.sort_index())
+        keys = r.json()
 
-    return pd.concat(dfs, join="outer", axis=1)
+        delta = dt.timedelta(days=1)
+        total_timespan = end_date - start_date
+        orig_start_date = start_date
 
+        fname = f"{prefix}-{entity_id}.csv"
 
-def get_asset_devices(client, asset):
-    """Get devices belonging to an asset."""
-    relations = rest_client.find_by_from(asset.id, asset.id.entity_type, "Contains")
-    devs = []
-    for rel in relations:
-        dev = rest_client.get_device_by_id(rel.to.id)
-        devs.append(dev)
+        logger.info(f"saving to {fname}")
 
-    return devs
+        # create the csv in write mode in the first iteration
+        # append in the following ones
+        # also only enable header in the first loop
+        append = False
 
+        while start_date <= end_date:
+            interval_start = start_date
+            interval_end = interval_start + delta
+            span = start_date - orig_start_date
+            start_date = interval_end
 
-def query_attributes(client, devices, query="", attributes=""):
-    """Query attributes from devices matching query."""
-    attrs = []
+            logger.info(f'{device["name"]}: fetching day {span.days+1} of {total_timespan.days+1} (from {interval_start.isoformat()} to {interval_end.isoformat()})')
 
-    try:
-        dev = [d for d in devices if query in d.name][0]
-        attrs = client.get_attributes(dev.id.entity_type, dev.id.id, attributes)
-        for attr in attrs:
-            if "Time" in attr["key"]:
-                attr["value"] = dt.datetime.fromtimestamp(attr["value"] / 1000.)
-    except IndexError:
-        pass
+            df = self.get_timeseries(device, keys, interval_start.timestamp(), interval_end.timestamp())
+            if df is None:
+                continue
 
-    return attrs
+            df.to_csv(fname, mode="a" if append else "w", columns=keys, header=not append)
+
+            append = True
+
+    def get_timeseries(self, device, keys, start_ts, end_ts):
+        """Get timeseries data from ThingsBoard using REST API and stores it into a Pandas dataframe."""
+        entity_id = device['id']['id']
+        entity_type = device['id']['entityType']
+        r = self._get(f'{self.url}/api/plugins/telemetry/{entity_type}/{entity_id}/values/timeseries',
+                      headers=self.auth_headers,
+                      params={'keys': ','.join(keys),
+                              'limit': 86400 * 3,
+                              'startTs': int(start_ts * 1000),
+                              'endTs': int(end_ts * 1000)})
+
+        if not r.json():
+            logger.warning("no data for selected interval")
+            return None
+
+        dfs = []
+        for k in r.json().keys():
+            df = pd.DataFrame.from_dict(r.json()[k])
+            df.set_index("ts", drop=True, inplace=True)
+            df.columns = [k]
+            dfs.append(df.sort_index())
+
+        return pd.concat(dfs, join="outer", axis=1)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="CA2020 ThingsBoard timeseries downloader",
+    parser = argparse.ArgumentParser(description="ThingsBoard timeseries downloader",
                                      formatter_class=lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog, max_help_position=8, width=100))
     parser.add_argument("--url", type=str, default="", help="ThingsBoard URL")
     parser.add_argument("--public-id", type=str, help="ThingsBoard public dashboard ID, login as Public Customer")
@@ -131,77 +196,48 @@ if __name__ == '__main__':
         logger.error("If both credentials types are provided, public login will be preferred")
         exit()
 
-    start_ts = int(dt.datetime.fromisoformat(args.start_date).timestamp() * 1000)
+        start_ts = int(dt.datetime.fromisoformat(args.start_date).timestamp() * 1000)
     end_ts = int(dt.datetime.fromisoformat(args.end_date).timestamp() * 1000)
 
     start_date = dt.datetime.fromisoformat(args.start_date)
     end_date = dt.datetime.fromisoformat(args.end_date)
 
-    with RestClientCE(base_url=args.url) as rest_client:
-        # TB has different API depending on what type of user you are (tenant, customer, other?)
-        # define a couple of helper methods to abstract those differences away
-        # assume we only have tenant users and a public customer, we'll get back to this if we device to enable other customers too
-        # I should probably subclass RestClientCE to make this more readable
-        if args.public_id:
-            _login = functools.partial(rest_client.public_login, args.public_id)
-            _get_assets = functools.partial(rest_client.get_customer_assets, args.public_id)
-            _get_devices = functools.partial(rest_client.get_customer_devices, args.public_id)
-        else:
-            _login = functools.partial(rest_client.login, username=args.username, password=args.password)
-            _get_assets = rest_client.get_tenant_assets
-            _get_devices = rest_client.get_tenant_devices
+    client = TBDownload(args.url,
+                        public_id=args.public_id,
+                        username=args.username,
+                        password=args.password)
+    client.login()
 
-        try:
-            logger.info(f"logging in to: {args.url}")
-            _login()
+    if args.list_devices:
+        assets = client.get_assets()
+        for asset in assets['data']:
+            if 'Main_' in asset['name']:
+                continue
 
-            if args.list_devices:
-                logger.info("getting assets")
-                assets = _get_assets('30', '0')
+            logger.info("---")
+            logger.info(f'asset: {asset["name"]}')
 
-                for asset in assets.data:
-                    if 'Main_' in asset.name:
-                        continue
+            devs = client.get_asset_devices(asset)
+            logger.info("devices: {}".format(", ".join([d['name'] for d in devs])))
 
-                    logger.info("---")
-                    logger.info(f'asset: {asset.name}')
+            attrs = client.query_attributes(devs, query="-gps",
+                                            attributes="station_name,station_location,active,lastActivityTime")
+            for attr in attrs:
+                logger.info(f'{attr["key"]}: {attr["value"]}')
 
-                    devs = get_asset_devices(rest_client, asset)
+        exit()
 
-                    # this is specific to our TB instance, each asset, which we
-                    # call station, has a gps device that publishes attributes
-                    # with some useful metadata about the station
-                    #
-                    # query and display them here for debugging purposes
-                    attrs = query_attributes(rest_client, devs,
-                                             query="-gps",
-                                             attributes="station_name,station_location,active,lastActivityTime")
-                    for attr in attrs:
-                        logger.info(f'{attr["key"]}: {attr["value"]}')
+    logger.info(f'querying devices matching search query: {args.query}')
+    devs = client.get_devices(text_search=args.query)
+    device_list = devs["data"]
 
-                    logger.info("devices: {}".format(", ".join([d.name for d in devs])))
+    if len(device_list) > 0:
+        logger.info("found {} devices matching the search query: {}".format(
+            len(device_list), ", ".join([d["name"] for d in device_list])))
+    else:
+        logger.warning("no devices matching search query: {}".format(args.query))
 
-                exit()
-
-            logger.info(f'querying devices matching search query: {args.query}')
-            devs = _get_devices('30', '0', text_search=args.query)
-            device_list = devs.data
-
-            if len(device_list) > 0:
-                logger.info("found {} devices matching the search query: {}".format(
-                    len(device_list),
-                    ", ".join([d.name for d in device_list])))
-            else:
-                logger.warning("no devices matching search query: {}".format(args.query))
-
-            for dev in device_list:
-                logger.info("---")
-                logger.info(f'downloading data for device: {dev.name}, id: {dev.id.id}')
-                save_timeseries(rest_client, dev, start_date, end_date, dev.name)
-
-        except ApiException as e:
-            logger.exception(e)
-
-        except KeyboardInterrupt:
-            logger.info("Caught CTRL+C, quitting.")
-            exit(1)
+    for dev in device_list:
+        logger.info("---")
+        logger.info(f'downloading data for device: {dev["name"]}, id: {dev["id"]["id"]}')
+        client.save_timeseries(dev, start_date, end_date, dev["name"])
