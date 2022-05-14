770b4247123230bd874683ddff353512126bb814
Filippo Argiolas <filippo.argiolas@gmail.com>
2022-05-14 17:57:41 +0200

    add some color to argparse help message
    playing with rich, mostly for fun.

--- tb_download/util.py
+++ tb_download/util.py
@@ -1,11 +1,13 @@
+"""Utils."""
 
+import argparse
 from rich.console import Console
 from rich.highlighter import RegexHighlighter
 from rich.theme import Theme
-
 from rich.traceback import install
 
-class MyHighlighter(RegexHighlighter):
+
+class TBDownloadHighlighter(RegexHighlighter):
     """Style!."""
 
     base_style = "base."
@@ -24,10 +26,10 @@ theme = Theme({"base.tag": "bold yellow",
                "base.date": "italic cyan",
                "base.path": "bold italic orange1"
                })
-console = Console(highlighter=MyHighlighter(), theme=theme, markup=True)
+console = Console(highlighter=TBDownloadHighlighter(), theme=theme, markup=True)
 
 
-def print_exception(*args, **kargs): # noqa: D103
+def print_exception(*args, **kwargs):  # noqa: D103
     return console.print_exception(*args, **kwargs)
 
 
@@ -45,3 +47,37 @@ def error(*args, **kwargs):  # noqa: D103
 
 def rule(*args, **kwargs):  # noqa: D103
     return console.rule(*args, **kwargs, style="purple")
+
+
+class ArgParseHighlighter(RegexHighlighter):
+    """Style for argparse help message."""
+
+    base_style = "base."
+    highlights = [r"(?P<short>-\w)[^\w]",
+                  r"(?P<long>--[\w-]+)[^\w]",
+                  r"--[\w-]+\s(?P<longarg>[A-Z_]+)",
+                  r"-[\w]\s(?P<shortarg>[A-Z_]+)",
+                  r"(?P<usage>usage:)",
+                  r"(?P<default>default:)",
+                  r"\(default:\s(?P<value>.*)\)",
+                  r"^usage:\s(?P<cmd>[\w_-]+)"
+                  ]
+
+
+argparse_theme = Theme({"base.short": "bold yellow",
+                        "base.long": "bold yellow",
+                        "base.shortarg": "bold magenta",
+                        "base.longarg": "bold purple",
+                        "base.usage": "bold",
+                        "base.cmd": "underline bold cyan",
+                        "base.default": "orange1",
+                        "base.value": "italic magenta",
+                        })
+argparse_console = Console(highlighter=ArgParseHighlighter(),
+                           theme=argparse_theme, markup=True)
+
+
+class RichArgumentParser(argparse.ArgumentParser):
+    """Apply rich styling to argparse messages."""
+    def _print_message(self, message: str, file=None):
+        argparse_console.print(message)
